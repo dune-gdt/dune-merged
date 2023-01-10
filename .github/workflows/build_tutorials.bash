47311ce1d3d333300c6990e99fe2fabda6ef0e99
René Fritze <rene.fritze@wwu.de>
2023-01-10 17:43:58 +0100

    [ci] upload tutorial html

---
+++ .github/workflows/build_tutorials.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# ~~~
+# This file is part of the dune-xt project:
+#   https://github.com/dune-community/dune-xt
+# Copyright 2009-2020 dune-xt developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
+# Authors:
+#   René Fritze    (2019)
+# ~~~
+
+set -ex
+
+THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ;  pwd -P )"
+OPTS_PATH=${THIS_DIR}/../../deps/config.opts/${CONFIG_OPTS}
+source ${OPTS_PATH}
+set -u
+
+DUNECONTROL=/src/deps/dune-common/bin/dunecontrol
+BUILD_CMD="ninja -v -j1"
+
+# TODO this is should be baked into the entrypoint
+. /venv/bin/activate
+
+ASAN_OPTIONS=${ASAN_OPTIONS} UBSAN_OPTIONS=${UBSAN_OPTIONS} LD_PRELOAD=${ASAN_LIB} ${DUNECONTROL} --opts=${OPTS_PATH} --only=dune-gdt bexec ${BUILD_CMD} tutorials
+
+[[ -d ${DUNE_BUILD_DIR}/testresults/tutorials/ ]] || mkdir -p ${DUNE_BUILD_DIR}/testresults/tutorials/
+cp ${DUNE_BUILD_DIR}/dune-gdt/tutorials/html ${DUNE_BUILD_DIR}/testresults/tutorials/
